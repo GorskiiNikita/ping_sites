0f9d24ec7690f730eb0d655a7f604847031baf4f
lev <nicki070898@gmail.com>
2020-06-15 00:58:43 +0300

    frontend

--- frontend/src/components/AddSiteForm.js
+++ frontend/src/components/AddSiteForm.js
@@ -5,15 +5,25 @@ class AddSiteForm extends Component {
     render() {
         return (
             <form className="adding-form" id="adding-form" method="POST"
-                  onSubmit={(e) => {this.props.onAdd(); e.preventDefault();}}>
-                <label>Название: <input type="text" name="name"/></label>
+                  onSubmit={(e) => {
+                      this.props.onAdd();
+                      this.clearAddingForm()
+                      e.preventDefault();
+                  }}>
+                <label>Название: <input type="text" name="name" size="25" required id="add-site-name"/></label>
                 <br />
-                <label>Адрес сайта: <input type="text" name="site" /></label>
+                <label>Адрес сайта: <input type="text" name="site" size="25" required id="add-site-url"/></label>
                 <br />
-                <label>Уведомления: <input type="checkbox" name="notification"/></label>
+                <label>Уведомления: <input type="checkbox" name="notification" id="add-site-notification"/></label>
                 <button>Добавить</button>
             </form>
         )
+    }
+
+    clearAddingForm() {
+        document.getElementById('add-site-name').value = '';
+        document.getElementById('add-site-url').value = '';
+        document.getElementById('add-site-notification').checked = true;
     }
 }
 
--- frontend/src/components/SiteItem.js
+++ frontend/src/components/SiteItem.js
@@ -18,9 +18,9 @@ class SiteItem extends Component {
                 <button className="delete-btn" onClick={this.props.onDelete}>Удалить</button>
                 <form className="settings-form" hidden id={this.state.formId} method="POST"
                       onSubmit={(e) => {this.props.onUpdate(); e.preventDefault();}}>
-                    <label>Название: <input type="text" defaultValue={this.props.siteItem.name} name="name"/></label>
+                    <label>Название: <input type="text" defaultValue={this.props.siteItem.name} name="name" size="35"/></label>
                     <br />
-                    <label>Адрес сайта: <input type="text" defaultValue={this.props.siteItem.site} name="site" /></label>
+                    <label>Адрес сайта: <input type="text" defaultValue={this.props.siteItem.site} name="site" size="35" /></label>
                     <br />
                     <label>Уведомления: <input type="checkbox" defaultChecked={this.props.siteItem.notification} name="notification"/></label>
                     <button>Сохранить</button>
