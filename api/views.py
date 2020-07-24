cc6b0a0db444d89b434ac262ce8716c11e58bcf9
lev <nicki070898@gmail.com>
2020-07-24 20:54:18 +0300

    fix returning data

--- api/views.py
+++ api/views.py
@@ -52,7 +52,7 @@ class CheckListViewSet(viewsets.ModelViewSet):
     def list(self, request, *args, **kwargs):
         queryset = CheckList.objects.filter(owner=request.user)
         serializer = CheckListSerializer(queryset, many=True)
-        return Response({'data': serializer.data[::-1]})
+        return Response({'data': serializer.data})
 
 
 @api_view(['POST'])
